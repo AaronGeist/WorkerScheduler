13c8624c625dc37e0668d3b45c9b435090aed155
Aaron Zhou <yzhou7@paypal.com>
2015-10-07 11:14:44 +0800

    Add free trial time check

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+# coding=utf-8
 from distutils.core import setup
 import py2exe
 
 __author__ = 'yzhou7'
 
+options = {'py2exe': {
+    "optimize": 2,
+    "compressed": 1
+}}
 setup(
     windows=[{
-         'script': 'Main.py',
-        'icon_resources': [(1, '.\Resource\panda.ico')]
-    }]
+        'script': 'Main.py',
+        'icon_resources': [(1, '.\Resource\panda.ico')],
+    }],
+    version='1.0',
+    zipfile=None,
+    options=options
 )
